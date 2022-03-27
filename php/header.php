50c6d89027d15413735287f4d6a4e492b62d06c1
not pug!! <67571892+d101ak11@users.noreply.github.com>
2022-03-27 13:30:18 +0700

    Create header.php

---
+++ php/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+	<link rel="stylesheet"  href="../fonts/themify-icons.css">
+	<link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="main_header">
+		<div class="header">
+			<div class="menu_top">
+				<a href="" class="text_1">Kênh Người Bán</a>
+				<a href="" class="text_2">Trở thành Người bán Shopee</a>
+				<a href="" class="text_3">Kết nối</a>
+				<i class="fab fa-facebook-square"></i><i class="fab fa-instagram-square"></i>
+				
+			</div>
+			<div class="header_right">
+					<a href="" class="text_4">Đăng Ký</a>
+					<a href="" class="text_5">Đăng Nhập</a>
+				</div>
+
+			<div class="menu_bottom">
+
+				<div>
+				<a href="./index.html"><img src="../img/icon_nho/logo.png" alt="anh loi"></a>
+				</div>
+
+				<div class="search_main">
+					<form role="search" class="shopee-searchbar-input" autocomplete="off">
+						<input  placeholder="Giảm ngay 500k thanh toán qua VPBank"  value="">
+					</form>
+				</div>
+				<button class="icon3">
+					<i class="fas fa-search"></i>
+				</button>
+
+				<i class="fas fa-shopping-cart "></i>
+
+				<div class="sub_menu">
+					<a href="../php/ao.php">Dép</a>
+					<a href="../php/ao.php">Áo Phông</a>
+					<a href="../php/ao.php">Váy</a>
+					<a href="../php/ao.php">Túi Xách Nữ</a>
+					<a href="../php/ao.php">Áo Croptop</a>
+					<a href="../php/ao.php">Áo Khoác</a>
+					<a href="../php/ao.php">Áo Thun</a>
+					<a href="../php/ao.php">Chân Váy</a>
+				</div>
+		</div>
+	</div>
+</body>
+</html>
