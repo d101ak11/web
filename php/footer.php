c07bca1d13449990c69dcd1de8168c4a8a093d06
not pug!! <67571892+d101ak11@users.noreply.github.com>
2022-03-27 13:30:17 +0700

    Create footer.php

---
+++ php/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+	<link rel="stylesheet"  href="../fonts/themify-icons.css">
+	<link rel="stylesheet" href="../css/stype.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="footer">
+		<div>
+			<h2>SHOPEE - GÌ CŨNG CÓ, MUA HẾT Ở SHOPEE</h2>
+			<p>Shopee - ứng dụng mua sắm trực tuyến thú vị, tin cậy, an toàn và miễn phí! Shopee là nền tảng giao dịch trực tuyến hàng đầu ở Đông Nam Á, Việt Nam, Singapore, Malaysia, Indonesia, Thái Lan, Philipin, Đài loan và Brazil. Với sự đảm bảo của Shopee, bạn sẽ mua hàng trực tuyến an tâm và nhanh chóng hơn bao giờ hết!</p>
+		</div>
+	</div>
+</body>
+</html>
