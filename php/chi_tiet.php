df2042309bd490e59869df4f47abcb22b2409602
not pug!! <67571892+d101ak11@users.noreply.github.com>
2022-03-27 13:30:07 +0700

    Create chi_tiet.php

---
+++ php/chi_tiet.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/chitiet.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="content_chitiet">
+        <h3>Chi tiết sản phẩm</h3>
+        <div class="chitiet">
+            <div class="chitiet__left">
+                <span>doanh mục</span>
+            </div>
+            <div class="chitiet__right">
+                <a href="../index.html">Shoppe</a>
+                <span>></span>
+                <a href="#">headPhone</a>
+            </div>
+        </div>
+        <div class="chitiet">
+            <div class="chitiet__left">
+                <span>kiểu kết nối</span>
+            </div>
+            <div class="chitiet__right">
+                <p>có dây</p>
+            </div>
+        </div>
+        <div class="chitiet">
+            <div class="chitiet__left">
+                <span>game chuyên dụng</span>
+            </div>
+            <div class="chitiet__right">
+                <p>có</p>
+            </div>
+        </div>
+        <div class="chitiet">
+            <div class="chitiet__left">
+                <span>thiết bị âm thanh</span>
+            </div>
+            <div class="chitiet__right">
+                <p>điện thoại di động Laptop , Pc</p>
+            </div>
+        </div>
+        <div class="chitiet">
+            <div class="chitiet__left">
+                <span>thể thao chuyen</span>
+            </div>
+            <div class="chitiet__right">
+                <p>có</p>
+            </div>
+        </div>
+        <div class="chitiet">
+            <div class="chitiet__left">
+                <span>kiểu kết nối</span>
+            </div>
+            <div class="chitiet__right">
+                <p>cổng kết nối phụ , Jack cắm headphoen</p>
+            </div>
+        </div>
+    </div>
+    <div class="motasanpham">
+        <h3>Mô tả sản phẩm</h3>
+        <p>Tai nghe gaming có mic chụp tai cao cấp với HD Mic và đèn LED
+           <br> Míc liền tai cứng cáp, chống đứt gãy
+           <br> Chuẩn cắm: 3.5mm
+           <br>Vòng loa: 50 mm
+           <br>Độ nhạy: 110dB S.P.L at 1Khzi
+           <br>Trở kháng: 32Ω – Tần số: 20Hz – 25kHz
+           <br>Kênh âm thanh: 2 kênh (Âm thanh nổi)
+           <br>Điện áp làm việc: DC 5V
+           <br>BẢO HÀNH 6 THÁNG</p>
+<p>Lưu ý: Sản phẩm có 3 dây cắm gồm dây micro – dây âm thanh – và cổng đèn led tai nghe hoạt động, <br> nên nếu cắm vào điện thoại hay máy chơi game không cắm cổng USB thì tai nghe sẽ không sáng</p>
+    </div>
+</body>
+</html>
