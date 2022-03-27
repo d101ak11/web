301171f7d24a99a9e181f9068ba1620194a09205
not pug!! <67571892+d101ak11@users.noreply.github.com>
2022-03-27 13:30:10 +0700

    Create comment.php

---
+++ php/comment.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/cmt.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="comment">
+        <h2>Đáng giá sản phẩm</h2>
+       <textarea name="" id="" cols="30" rows="10" placeholder="Commment tại đây"></textarea>
+       <br>
+       <button>Submit</button>
+    </div>
+</body>
+</html>
