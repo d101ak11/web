01d7c8e54392e00f55a86534004cdb9c285c2192
not pug!! <67571892+d101ak11@users.noreply.github.com>
2022-03-27 13:30:20 +0700

    Create sanphamtuongtu.php

---
+++ php/sanphamtuongtu.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="content_4">
+				<ul>
+					<li>
+						<h2>Sản phẩm tương tự</h2>
+					</li>
+				</ul>
+
+				<div>
+					<ul>
+						<li>
+							<img src="../img/icon_nho/28.jpg" alt="">
+							<p>Áo thun planned farmer </p>
+							<p> áo phông nam nữ giá sốc</p>
+							<p class="price">₫12.000 - ₫59.000</p>
+						</li>
+						<li><img src="../img/icon_nho/29.jpg" alt="">
+							<p>Áo sweater nam nữ</p>
+							<p>áo hoodie form rộng tay dài</p>
+							<p class="price">₫1.400</p>
+						</li>
+						<li><img src="../img/icon_nho/30.jpg" alt="">
+							<p>HÀNG CAO CẤP</p>
+							<p>Dép nam,Dép lê nam nữ</p>
+							<p class="price">₫1.326</p>
+						</li>
+						<li><img src="../img/icon_nho/31.jpg" alt="">
+							<p>Giày MLB NY Cao Cấp</p>
+							<p>Hàng Cao Cấp Full Box</p>
+							<p class="price">₫12.000</p>
+						</li>
+						<li><img src="../img/icon_nho/32.jpg" alt="">
+							<p>Áo Thun Nam, Nữ Rộng</p>
+							<p>Unisex Basic Cotton</p>
+							<p class="price">₫19.999</p>
+						</li>
+						<li><img src="../img/icon_nho/33.jpg" alt="">
+							<p>Áo sweater nam nữ unisex</p>
+							<p>áo hoodie form rộng tay dài</p>
+							<p class="price">₫1.000</p>
+						</li>
+
+					</ul>
+				</div>
+
+				<div>
+					<ul>
+						<li>
+							<img src="../img/icon_nho/34.jpg" alt="">
+							<p>Giày đốc nữ da bóng</p>
+							<p> ,da lỳ phối nơ to</p>
+							<p class="price">₫85.000 - ₫89.000</p>
+						</li>
+						<li><img src="../img/icon_nho/33.jpg" alt="">
+							<p>Áo sweater nam nữ</p>
+							<p>áo hoodie form rộng tay dài</p>
+							<p class="price">₫19.999</p>
+						</li>
+						<li><img src="../img/icon_nho/35.jpg" alt="">
+							<p>Áo thun tay lỡ form rộng</p>
+							<p>cotton oversize</p>
+							<p class="price">₫1.250</p>
+						</li>
+						<li><img src="../img/icon_nho/36.jpg" alt="">
+							<p>áo phông unisex </p>
+							<p>thấm hút mồ hôi</p>
+							<p class="price">₫1.200</p>
+						</li>
+						<li><img src="../img/icon_nho/37.jpg" alt="">
+							<p>Giày Jordan cổ thấp</p>
+							<p>màu xanh ngọc nam nữ</p>
+							<p class="price">₫1.326</p>
+						</li>
+						<li><img src="../img/icon_nho/38.jpg" alt="">
+							<p>Ốp Điện Thoại iPhone</p>
+							<p>6/6Plus/6S/6SPlus...</p>
+							<p class="price">19.999</p>
+						</li>
+
+					</ul>
+				</div>
+
+				<div>
+					<ul>
+						<li>
+							<img src="../img/icon_nho/39.jpg" alt="">
+							<p>áo phông unisex form rộng </p>
+							<p>cotton mát mịn thấm hút </p>
+							<p class="price">₫12.000 - ₫59.000</p>
+						</li>
+						<li><img src="../img/icon_nho/40.jpg" alt="">
+							<p>Giày Jordan cổ thấp</p>
+							<p>màu xanh ngọc nam nữ</p>
+							<p class="price">₫1.326</p>
+						</li>
+						<li><img src="../img/icon_nho/41.jpg" alt="">
+							<p>Quần legging nữ lửng cạp</p>
+							<p>quần tregging đùi ngố </p>
+							<p class="price">₫1.400</p>
+						</li>
+						<li><img src="../img/icon_nho/42.jpg" alt="">
+							<p>Áo thun unisex</p>
+							<p>from rộng tay</p>
+							<p class="price">₫1.000</p>
+						</li>
+						<li><img src="../img/icon_nho/43.jpg" alt="">
+							<p>Áo thun nam nữ unisex</p>
+							<p>oversize streetwear</p>
+							<p class="price">₫12.000 - ₫59.000</p>
+						</li>
+						<li><img src="../img/icon_nho/44.jpg" alt="">
+							<p>Dép độn đê tăng chiều cao</p>
+							<p>quai ngang đúc nguyên khối</p>
+							<p class="price">₫1.000</p>
+						</li>
+
+					</ul>
+				</div>
+			</div>
+</body>
+</html>
