ed48402d4550df1f8e15cf1ac1a0ef502e23489e
not pug!! <67571892+d101ak11@users.noreply.github.com>
2022-03-27 13:30:12 +0700

    Create connect.php

---
+++ php/connect.php
@@ -0,0 +1,16 @@
+<!-- ket noi database -->
+<?php 
+    $server = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $database = 'comment';
+    $conn = new mysqLi($server , $user , $pass ,$database);
+    if($conn)
+    {
+        mysqLi_query($conn, "SET NAMES 'utf8' ");
+        echo 'ket noi thanh cong';
+    }
+    else {
+        echo 'ket noi that bai';
+    }
+?>
