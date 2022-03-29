7b2226f4489e4d169804a11b5b3927afc3c2b1a1
not pug!! <67571892+d101ak11@users.noreply.github.com>
2022-03-29 08:53:07 +0700

    Update comment.php

--- php/comment.php
+++ php/comment.php
@@ -10,6 +10,30 @@
 <body>
     <div class="comment">
         <h2>Đáng giá sản phẩm</h2>
+        <div class="cmt">
+        <img src="../img/anh1.jpg" alt="">
+           <div id="cmt">
+                <h3>Đức NGuyễn</h3>
+            <p> sản phẩm tốt hơn mong đợi</p></div>
+        </div>
+        <div class="cmt">
+        <img src="../img/anh2.jpg" alt="">
+           <div id="cmt">
+                <h3>not Pug</h3>
+            <p>hàng ok , chất lượng cao mọn người lên dùng thử nhé</p></div>
+        </div>
+        <div class="cmt">
+        <img src="../img/anh3.jpg" alt="">
+           <div id="cmt">
+                <h3>Hà</h3>
+            <p> Hàng đạt chuẩn , phù hợp với giá tiền </p></div>
+        </div>
+        <div class="cmt">
+        <img src="../img/anh4.jpg" alt="">
+           <div id="cmt">
+                <h3>lolicon</h3>
+            <p> sản phẩm tốt  tai nghe chất lượng cao</p></div>
+        </div>
        <textarea name="" id="" cols="30" rows="10" placeholder="Commment tại đây"></textarea>
        <br>
        <button>Submit</button>
