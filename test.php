02ec4b766c9fa6b402bd5df6e3aafc819ee8d077
not pug!! <67571892+d101ak11@users.noreply.github.com>
2022-03-29 08:52:55 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/cmt.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="comment">
+        <h2>Đáng giá sản phẩm</h2>
+        <div class="cmt">
+        <img src="../img/anh2.jpg" alt="">
+           <div id="cmt">
+                <h3>not Pug</h3>
+            <p>hàng ok , chất lượng cao mọn người lên dùng thử nhé</p></div>
+        </div>
+        <div class="cmt">
+        <img src="../img/anh3.jpg" alt="">
+           <div id="cmt">
+                <h3>Hà</h3>
+            <p> Hàng đạt chuẩn , phù hợp với giá tiền </p></div>
+        </div>
+        <div class="cmt">
+        <img src="../img/anh4.jpg" alt="">
+           <div id="cmt">
+                <h3>lolicon</h3>
+            <p> sản phẩm tốt  tai nghe chất lượng cao</p></div>
+        </div>
+       <textarea name="" id="" cols="30" rows="10" placeholder="Commment tại đây"></textarea>
+       <br>
+       <button>Submit</button>
+    </div>
+</body>
+</html>
